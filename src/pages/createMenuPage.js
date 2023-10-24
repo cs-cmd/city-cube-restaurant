e154ddcf49f56124c75fa6db7bb5d48ae9f3424a
Ray <rayoneill.97@gmail.com>
2023-10-24 16:38:08 -0400

    Added grid view in 'menu' page where menu items will be placed; adjusted imaged inside of grid cells to scale height according to width (aspect ratio);

--- src/pages/createMenuPage.js
+++ src/pages/createMenuPage.js
@@ -1,4 +1,5 @@
 import { createLink, setUpBaseDiv } from "./createBaseline";
+import '../../res/styles/menu-grid-style.css';
 
 export function createMenuPage() {
     let mainDiv = setUpBaseDiv();
@@ -14,6 +15,24 @@ export function createMenuPage() {
     p1.innerHTML += contactLink.outerHTML;
     p1.innerHTML += ' to see about adding your idea to our ever-growing menu!';
     mainDiv.appendChild(p1);
+
+    const menu = document.createElement('div');
+    menu.classList.add('menu');
+
+    for(let i = 0; i < 6; ++i) {
+        const cell = document.createElement('figure');
+        cell.classList.add('cell');
+        const image = new Image();
+        // image.src = src;
+        const figCap = document.createElement('figcaption');
+        figCap.innerText = 'item';
+        cell.appendChild(image);
+        cell.appendChild(figCap);
+
+        menu.appendChild(cell);
+    }
+
+    mainDiv.appendChild(menu);
 
     return mainDiv;
 }
