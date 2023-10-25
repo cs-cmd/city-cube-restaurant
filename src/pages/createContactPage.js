6ad11f911910aff27ab98c4991017544d0ce7157
Ray O'Neill <rayoneill.97@gmail.com>
2023-10-24 21:04:37 -0400

    Imported image assets for use with various page tabs; Added a 'TODO' file to list the issues that need to be resolved (will be replaced with a better task-manager);

--- src/pages/createContactPage.js
+++ src/pages/createContactPage.js
@@ -3,6 +3,7 @@ import { setUpBaseDiv } from "./createBaseline";
 export function createContactPage() {
     const mainDiv = setUpBaseDiv();
     mainDiv.innerText = 'Hello contact page!';
+    mainDiv.style.textAlign = 'center';
     
     return mainDiv;
 }
