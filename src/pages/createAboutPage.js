7807e735828e1c4dfcaa9f9415f2b5e73215eaaf
Ray <rayoneill.97@gmail.com>
2023-10-25 09:24:15 -0400

    Changed layout of home page; added link to source code; added SVG for source code link icon; edited layout of default, 'under construction' pages;

--- src/pages/createAboutPage.js
+++ src/pages/createAboutPage.js
@@ -2,8 +2,18 @@ import { setUpBaseDiv } from "./createBaseline";
 
 export function createAboutPage() {
     const mainDiv = setUpBaseDiv();
-    mainDiv.innerText = 'Hello about page!';
 
+    const h1 = document.createElement('h1');
+    h1.style.textAlign = 'center';
+    h1.innerText = ':: \'About Page\' is under construction ::';
+
+    const h3 = document.createElement('h3');
+    h3.style.textAlign = 'center';
+    h3.innerText = ':: Please stand by ::';
+
+
+    mainDiv.appendChild(h1);
+    mainDiv.appendChild(h3);
 
     return mainDiv;
 }
--- src/pages/createContactPage.js
+++ src/pages/createContactPage.js
@@ -2,8 +2,18 @@ import { setUpBaseDiv } from "./createBaseline";
 
 export function createContactPage() {
     const mainDiv = setUpBaseDiv();
-    mainDiv.innerText = 'Hello contact page!';
-    mainDiv.style.textAlign = 'center';
+
+    const h1 = document.createElement('h1');
+    h1.style.textAlign = 'center';
+    h1.innerText = ':: \'Contact Page\' is under construction ::';
+
+    const h3 = document.createElement('h3');
+    h3.style.textAlign = 'center';
+    h3.innerHTML = ':: Please stand by ::';
+
+    mainDiv.appendChild(h1);
+    mainDiv.appendChild(h3);
+
     
     return mainDiv;
 }
